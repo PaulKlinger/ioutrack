b0b993cf512c1f299f31a6c89c3f46b12fbd34ff
Paul Klinger <klinger.paul@gmail.com>
2022-05-29 00:12:01 +0100

    fixed benches (added noise parameters)

--- benches/mot.rs
+++ benches/mot.rs
@@ -4,7 +4,14 @@ use ndarray::prelude::*;
 use ndarray_npy::read_npy;
 
 fn run_sort_on_dets(dets: &Array2<f32>, frame_borders: &Array1<usize>) {
-    let mut tracker = SORTTracker::new(25, 2, 0.3, 0.5);
+    let mut tracker = SORTTracker::new(
+        25,
+        2,
+        0.3,
+        0.5,
+        [1., 1., 10., 10.],
+        [1., 1., 1., 1., 0.01, 0.01, 0.0001],
+    );
     let mut first_i: usize = 0;
     for &last_i in frame_borders.iter() {
         tracker
@@ -26,7 +33,16 @@ pub fn criterion_mot_sort_benchmark(c: &mut Criterion) {
 }
 
 fn run_bytetrack_on_dets(dets: &Array2<f32>, frame_borders: &Array1<usize>) {
-    let mut tracker = ByteTrack::new(25, 2, 0.3, 0.5, 0.5, 0.1);
+    let mut tracker = ByteTrack::new(
+        25,
+        2,
+        0.3,
+        0.5,
+        0.5,
+        0.1,
+        [1., 1., 10., 10.],
+        [1., 1., 1., 1., 0.01, 0.01, 0.0001],
+    );
     let mut first_i: usize = 0;
     for &last_i in frame_borders.iter() {
         tracker
